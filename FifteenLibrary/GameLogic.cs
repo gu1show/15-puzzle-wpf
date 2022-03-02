3ba9346b8577eae8d0bebd927fec21ed2e11a3bb
gu1show <denissolovyov57@gmail.com>
2022-03-02 22:18:53 +0300

    First commit

---
+++ FifteenLibrary/GameLogic.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FifteenGUI
+{
+    // Originator
+    public class Game
+    {
+        int[,] field;
+        int size, x0, y0;
+        Random rand = new Random();
+
+        public Game(int size)
+        {
+            this.size = size;
+            field = new int[size, size];
+        }
+
+        private int TurnCoordinatesToPosition(int x, int y)
+        {
+            return 4 * y + x;
+        }
+
+        public void TurnPositionToCoordinates(int position, out int x, out int y)
+        {
+            x = Math.Abs(position % 4);
+            y = Math.Abs(position / 4);
+        }
+
+        public void Start()
+        {
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    field[i, j] = TurnCoordinatesToPosition(i, j) + 1;
+
+            field[3, 3] = 0;
+            x0 = 3;
+            y0 = 3;
+        }
+
+        public int GetNumber(int position)
+        {
+            int x, y;
+            TurnPositionToCoordinates(position, out x, out y);
+
+            if ((x > -1) && (x < 4) && (y > -1) && (y < 4)) return field[x, y];
+            else return 0;
+        }
+
+        public bool CanShift(int x, int y)
+        {
+            if (((Math.Abs(x0 - x) < 2) && (y0 - y == 0)) || ((x0 - x == 0) && (Math.Abs(y0 - y) < 2)))
+                return true;
+            else return false;
+        }
+
+        public void Shift(int x, int y)
+        {
+            field[x0, y0] = field[x, y];
+            field[x, y] = 0;
+            x0 = x;
+            y0 = y;
+        }
+
+        public void ShiftRandom()
+        {
+            int move = rand.Next(4), x = x0, y = y0;
+
+            if (move == 0) x = Math.Abs(x - 1) % 4;
+            else if (move == 1) x = Math.Abs(x + 1) % 4;
+            else if (move == 2) y = Math.Abs(y - 1) % 4;
+            else y = Math.Abs(y + 1) % 4;
+
+            if (CanShift(x, y)) Shift(x, y);
+        }
+
+        public bool CheckWin()
+        {
+            if (field[3, 3] != 0) return false;
+            else return CheckEachPosition();
+        }
+
+        private bool CheckEachPosition()
+        {
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    if (
+                        (field[i, j] != TurnCoordinatesToPosition(i, j) + 1) &&
+                        (((i != 3) || (j != 3)))
+                       )
+                        return false;
+            return true;
+        }
+
+        public GameMemento SaveState()
+        {
+            return new GameMemento(field, x0, y0);
+        }
+
+        public void RestoreState(GameMemento memento)
+        {
+            this.field = memento.Field;
+            this.x0 = memento.x0;
+            this.y0 = memento.y0;
+        }
+    }
+
+    // Memento
+    public class GameMemento
+    {
+        public int[,] Field { get; private set; }
+        public int x0 { get; private set; }
+        public int y0 { get; private set; }
+
+        public GameMemento(int[,] field, int x0, int y0)
+        {
+            Field = new int[4, 4];
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    Field[i, j] = field[i, j];
+
+            this.x0 = x0;
+            this.y0 = y0;
+        }
+    }
+
+    // Caretaker
+    public class GameHistory
+    {
+        public Stack<GameMemento> History { get; private set; }
+
+        public GameHistory()
+        {
+            History = new Stack<GameMemento>();
+        }
+    }
+}
+
---
+++ FifteenWPF/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace FifteenWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        FifteenGUI.Game game;
+        FifteenGUI.GameHistory gameHistory;
+        DispatcherTimer timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            game = new FifteenGUI.Game(4);
+            gameHistory = new FifteenGUI.GameHistory();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += new EventHandler(timer_Tick);
+        }
+
+        private Button GetButton(int index)
+        {
+            return (Button)FindName("button" + index);
+        }
+
+        private void RefreshButtonField()
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                Button tempButton = GetButton(i);
+                tempButton.Content = game.GetNumber(i).ToString();
+                String tempString = tempButton.Content.ToString();
+                if (tempString == "0") tempButton.Visibility = Visibility.Hidden;
+                else tempButton.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            StartNewGame();
+        }
+
+        private void MenuStartGame_Click(object sender, RoutedEventArgs e)
+        {
+            StartNewGame();
+        }
+
+        private void StartNewGame()
+        {
+            MenuCancelMyTurn.Visibility = Visibility.Hidden;
+            timer.Stop();
+            game.Start();
+            for (int i = 0; i < 100; i++) game.ShiftRandom();
+            RefreshButtonField();
+            labelTime.Content = "0";
+            labelScore.Content = "0";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+            int position = Convert.ToInt32(((Button)sender).Tag);
+
+            int x, y;
+            game.TurnPositionToCoordinates(position, out x, out y);
+            if (game.CanShift(x, y))
+            {
+                gameHistory.History.Push(game.SaveState());
+                game.Shift(x, y);
+                labelScore.Content = $"{Convert.ToInt32(labelScore.Content) + 1}";
+                RefreshButtonField();
+                MenuCancelMyTurn.Visibility = Visibility.Visible;
+            }
+
+            if (game.CheckWin())
+            {
+                timer.Stop();
+                MessageBox.Show("You win the game!", "Information");
+            }
+        }
+
+        private void CancelMyTurn_Click(object sender, RoutedEventArgs e)
+        {
+            CancelTurn();
+        }
+
+        private void CancelTurn()
+        {
+            if (gameHistory.History.Count > 0)
+            {
+                game.RestoreState(gameHistory.History.Pop());
+                if (gameHistory.History.Count == 0) MenuCancelMyTurn.Visibility = Visibility.Hidden;
+                RefreshButtonField();
+            }
+        }
+
+        public void timer_Tick(object sender, EventArgs e)
+        {
+            labelTime.Content = $"{Convert.ToInt32(labelTime.Content) + 1}";
+        }
+
+        private void DockPanel_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.Key == Key.Z) && (e.KeyboardDevice.Modifiers == ModifierKeys.Control)) CancelTurn();
+        }
+    }
+}
